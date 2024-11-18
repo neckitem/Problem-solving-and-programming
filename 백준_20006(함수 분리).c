26f48dd390aff83714973afbb7cadd4bf377e125
neckitem <awdetff029@gmail.com>
2024-11-19 01:41:06 +0900

    update

--- 백준_20006(함수 분리).c
+++ 백준_20006(함수 분리).c
@@ -5,7 +5,7 @@
 char player_name[17], char_team[300][300][17], char_temp[17];
 int player_num, team_num, player_level, int_team[300][300], team_cnt = 0, team_idx[300] = { 0. }, i, j, k, check = 0, int_temp;
 
-int input(player_num, team_num) {
+void input(player_num, team_num) {
 	for (i = 0; i < player_num; i++) {
 		scanf("%d %s", &player_level, player_name);
 		if (i == 0) {
@@ -36,7 +36,7 @@ int input(player_num, team_num) {
 	}
 }
 
-int Array() {
+void Array() {
 	for (i = 0; i < team_cnt; i++) {
 		for (j = 0; j < team_idx[i]; j++) {
 			for (k = j; k < team_idx[i] - 1; k++) {
@@ -54,7 +54,7 @@ int Array() {
 	}
 }
 
-int output() {
+void output() {
 	for (i = 0; i < team_cnt; i++) {
 		if (team_idx[i] == team_num) printf("Started!\n");
 		else printf("Waiting!\n");
